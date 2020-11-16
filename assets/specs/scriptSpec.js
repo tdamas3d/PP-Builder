565b3c618bb9236c8c249ba730c21fd07f206abe
Jean <tdamas3d@gmail.com>
2020-11-16 12:17:50 +0000

    Linking to JQUERRY libraries for javascript

--- assets/specs/scriptSpec.js
+++ assets/specs/scriptSpec.js
@@ -3,5 +3,13 @@ describe("Addition tests", function(){
  it("should return 42", function() {
 expect(addition(20, 22)).toBe(42);
 });
+it("should return 26", function() {
+expect(addition(7, 19)).toBe(26);
+});
+
+it("should return an error if we supply no number arguments", function() {
+expect(addition("Hello Word", "Message")).toBe("Error");
+});
+
 });
 });
