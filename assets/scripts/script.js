c0d4f79d510579c77b7f3c57cd32c9900a96b447
Jean <tdamas3d@gmail.com>
2020-11-16 19:58:55 +0000

    Add - Added the search products form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,20 @@
-function addition(numOne, numTwo){
+/*function addition(numOne, numTwo){
     if(typeof(numOne)=="number" &&typeof(numTwo)=="number"){
     return(numOne+numTwo);
     } else return("Error");
-}
+}*/
+
+$(document).ready(function(){
+/*
+$("#user-support").over(function(){
+$("#user-support").css("font-size","11px");
+});*/
+
+/*
+var defaultText=$(".social-networks").html();
+$("body").append(defaultText);
+alert(defaultText);*/
+
+
+}); /*end of jquery scripts*/
+
--- assets/specs/scriptSpec.js
+++ assets/specs/scriptSpec.js
@@ -1,4 +1,4 @@
-describe("Calculator", function(){
+/*describe("Calculator", function(){
 describe("Addition tests", function(){
  it("should return 42", function() {
 expect(addition(20, 22)).toBe(42);
@@ -12,4 +12,4 @@ expect(addition("Hello Word", "Message")).toBe("Error");
 });
 
 });
-});
+});*/
