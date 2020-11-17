4413bad36b49ccbb91d292e4df3a3b399ce986fe
Jean <tdamas3d@gmail.com>
2020-11-17 16:51:17 +0000

    Add - Created a dropdown menu for Resources using Jquery

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,31 +1,57 @@
-/*function addition(numOne, numTwo){
-    if(typeof(numOne)=="number" &&typeof(numTwo)=="number"){
-    return(numOne+numTwo);
-    } else return("Error");
-}*/
 
+/*Start Javascript Developer code */
+/* Enclosing Our javascript into javascript's DOM "ready" function for the script to run after the entire html oage is loaded  */
 $(document).ready(function(){
 
-/*
-$("#searchProducts").click(function(){
-$("#user-support").css("font-size","11px");
+$("#user-support").hover(function(){ 
+    $("#support-down-menu").removeClass('top-nav-links');
+    $("#support-down-menu").addClass('resetMenu');
+    $("#support-down-menu").addClass('dropDownMenu');
+    $("#support-down-menu").show();
+    $("#home-top-menu-item").hide();
+    $("#login-top-menu-item").hide();
   });
-  */
+  
+$("#support-down-menu").mouseout(function(){
+    $("#support-down-menu").hide();
+        $("#home-top-menu-item").show();
+    $("#login-top-menu-item").show();
+  });
+  
+  /*Create a drop down menu for the Resources Menu Option - it has subsections that we would like to hide by default and show them on mouse over*/
+$("#resourcesItem").hover(function(){
+    $("#drop-down-menu").show();
+  });
+
+
+$("#drop-down-menu").hover(function(){
+        $("#resourcesItem").show();
+        $("#drop-down-menu").show();
+  });
+
+    $("#resourcesItem").mouseleave(function(){
+        $("#resourcesItem").show();
+        $("#drop-down-menu").hide();
+  });
+
+  $("#drop-down-menu").mouseleave(function(){
+        $("#resourcesItem").show();
+        $("#drop-down-menu").hide();
+  });
+
+
+
+
+
+
   
   $("#searchProducts").click($.fn.searchSite=function(){
     var url1=$("#siteUrl").val();/* Site to search products from*/
     var url2=$("#productName").val(); /* keywords, name or type of products */
     var url=url1+url2; /* combined or the url of the search */
-    $("#productsSearchForm").attr("action",url);
-       
-    /*return false;*/
+    $("#productsSearchForm").attr('action',url);
 
   });
-
-/*
-var defaultText=$(".social-networks").html();
-$("body").append(defaultText);
-alert(defaultText);*/
 
 
 }); /*end of jquery scripts*/
